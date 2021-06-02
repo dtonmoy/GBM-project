2548f94652c10cdb4212cbe31441c02d07f3ed9c
Tonmoy Das <40005678+dtonmoy@users.noreply.github.com>
2021-06-02 12:12:15 +0600

    Update Heatmap_inferno.R

--- Scripts/Heatmap_inferno.R
+++ Scripts/Heatmap_inferno.R
@@ -7,20 +7,10 @@ library(RColorBrewer)
 library(viridis)
 library(openxlsx)
 
-### Figure 1B
-mat = read.xlsx("1B.xlsx",rowNames = T)
-mat = t(mat)
-pheatmap(mat, show_colnames = T, 
-         fontsize_row = 10, fontsize_col = 10,
-         angle_col = 90,
-         cluster_cols = F,
-         cluster_rows = F,
-         color = inferno(10),
-         cellwidth = 10, cellheight = 10, border_color = 'grey')
+# import matrix
+mat = read.xlsx(file.choose(), rowNames = T) # if the input is an excel file
+mat = read.csv(file.choose(), row.names = 1) # if the input is a csv file
 
-
-### Figure 3A
-mat = read.csv("3A.csv",row.names = 1)
 mat[is.na(mat)] = 0
 mat = t(mat)
 pheatmap(mat,  show_colnames = T, 
@@ -32,14 +22,3 @@ pheatmap(mat,  show_colnames = T,
          color = inferno(100),
          cellwidth = 10, cellheight = 10, border_color = 'grey')
 
-
-### Supplementary figure 2K
-mat = read.xlsx("Sup 2K.xlsx", rowNames = T)
-mat = t(mat)
-pheatmap(mat, show_colnames = T, 
-         fontsize_row = 10, fontsize_col = 10,
-         angle_col = 90,
-         cluster_cols = F,
-         cluster_rows = F,
-         color = inferno(10),
-         cellwidth = 10, cellheight = 10, border_color = 'grey')
